5d90189e4375b1faf06b7b3bd45cd0e3c5ce3e77
Keegan Davis <Keegster11@gmail.com>
2019-02-12 08:05:51 -0600

    Updating Files

--- src/javabug/Main.java
+++ src/javabug/Main.java
@@ -1,10 +1,14 @@
 package javabug;
 
-public class Main {
-
-	public static void main(String[] args) {
+class Main
+{
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-System.out.println("Hello");
+		//System.out.println("Hello");
+		
+		Stage myStage = new Stage();
+		System.out.println(myStage.draw());
+		
 	}
-
 }
